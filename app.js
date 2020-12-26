7cd298e0ad44098436490f2c3ce072cb3c353cc4
Stan Sob <stan@gofar.co>
2020-12-26 18:44:49 +0100

    configure database app

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+const path = require('path');
 const express = require('express');
+const database = require('./utils/database');
 
 const app = express();
-
 
 app.use((req, res, next) => {
     res.status(200).json({"message": "Hello World"});
  });
 
 
- app.listen(process.env.PORT || 3000);
+ database.database.startRun( () => {
+    app.listen(process.env.PORT || 3000);
+}, (error) => {
+    console.log(err);
+})
 
---
+++ utils/database.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('d6ffj1t93fs2po','pjzvciwbdnbghn','d85a0866e870bd18a891645a0da465cd6e4ddeaa352c4e36add6f6a47765f657', {
+    host: 'ec2-54-247-71-245.eu-west-1.compute.amazonaws.com',
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false 
+          }
+    } 
+});
+
+class Database {
+
+    static startRun(successCallback, failureCallBack) {
+        sequelize.sync()
+        .then (user => {
+            successCallback();
+        }).catch( err => {
+            console.log(err);
+            failureCallBack(err);
+        });     
+    }
+}
+
+exports.database = Database;
+exports.sequelize = sequelize;
